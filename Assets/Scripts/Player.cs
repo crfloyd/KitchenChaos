d159a3860192b7e07165542e2b5439027ae17eeb
corey <fourfloyds@gmail.com>
2023-07-10 19:12:29 -0500

    updates to new unity input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,35 +7,13 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-
-        }
-
-        inputVector.Normalize();
+        var inputVector = gameInput.GetMovementVectorNormalized();
 
         var direction = new Vector3(inputVector.x, 0f, inputVector.y);
 
